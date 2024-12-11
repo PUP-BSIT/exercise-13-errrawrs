d4bac47c8e00275eeb718ae4bb914c0d63fd4f55
delumen-ivan <ivandelumen05@gmail.com>
2024-12-11 14:11:18 +0800

    upload delumen module

---
+++ package/delumen.py
@@ -0,0 +1,16 @@
+def greetings():
+    print("Hello, Ivan V. Delumen ")
+    
+def basic_info():
+    print("Name: Ivan V. Delumen")
+    print("Age: 20")
+    print("Birthdate: October 28, 2004")
+    print("Hometown: Taguig City")
+
+def goals():
+    print("I want to be a front end developer.")
+    print("To be a successful person.")
+
+greetings()
+basic_info()
+goals()
