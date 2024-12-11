fd65754fd599f695b439999060b805f61d6b0768
delumen-ivan <ivandelumen05@gmail.com>
2024-12-11 14:27:48 +0800

    update delumen module

--- package/delumen.py
+++ package/delumen.py
@@ -1,5 +1,12 @@
-def greetings():
-    print("Hello, Ivan V. Delumen ")
+def menu():
+    print("Hello, Ivan V. Delumen")
+    print("1)Basic Information")
+    print("2) Goals")
+    print("3) Comment from Mamasalanang")
+    print("4) Comment from Mosquito")
+    print("5) Comment from Reyes")
+    print("6) Comment from Victorioso")
+    print("7: Exit")
     
 def basic_info():
     print("Name: Ivan V. Delumen")
@@ -11,6 +18,38 @@ def goals():
     print("I want to be a front end developer.")
     print("To be a successful person.")
 
-greetings()
-basic_info()
-goals()
+def comment_mamasalanang():
+    print("")
+
+def comment_mosquito():
+    print("")
+
+def comment_reyes():
+    print("")
+
+def comment_victorioso():
+    print("")
+
+def menu_function():
+    while True: 
+        menu()
+
+        user_choice = input("Enter your choice: ")
+
+        if user_choice == "1":
+            basic_info()
+        elif user_choice == "2":
+            goals()
+        elif user_choice == "3":
+            comment_mamasalanang()
+        elif user_choice == "4":
+            comment_mosquito()
+        elif user_choice == "5":
+            comment_reyes
+        elif user_choice == "6":
+            comment_victorioso()
+        elif user_choice == "7":
+            print("Exit.")
+            break
+        else: 
+            print("Invalid Input!")
